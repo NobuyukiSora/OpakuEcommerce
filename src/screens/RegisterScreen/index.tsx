ec38fdda36f1ae7a448764e3d0fccecfebfb10ef
Jessen Kanigara <blank.sora.21@gmail.com>
2025-03-24 23:58:44 +0700

    feat register

---
+++ src/screens/RegisterScreen/index.tsx
@@ -0,0 +1,75 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import analytics from '@react-native-firebase/analytics';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import React, {useMemo} from 'react';
+import {Image, TouchableOpacity, View} from 'react-native';
+import Config from 'react-native-config';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Metrics, themeColors, Typograph} from 'soras-ui';
+import Navigator from '../../navigation/Navigator';
+import {createStyles} from './styles';
+
+const RegisterScreen = () => {
+  const insets = useSafeAreaInsets();
+  const styles = useMemo(
+    () => createStyles(Metrics, insets, themeColors),
+    [themeColors],
+  );
+
+  const registerUser = async () => {
+    await analytics().logEvent('user_registered', {
+      method: 'email_password',
+    });
+    signInWithGoogle();
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      const idToken = userInfo?.data?.idToken;
+
+      if (!idToken) {
+        throw new Error('Google Sign-In failed: No idToken received');
+      }
+
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      await AsyncStorage.setItem('TOKEN', idToken);
+      await auth().signInWithCredential(googleCredential);
+      Navigator.pureReset('Home');
+    } catch (error) {
+      console.error('Google Sign-In Error:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../../assets/images/Art.Logo.png')}
+          height={200}
+          width={Metrics.screenWidth * 0.9}
+          style={{width: Metrics.screenWidth * 0.9, resizeMode: 'contain'}}
+        />
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={() => registerUser()}
+          style={styles.buttonLogin}>
+          <Image
+            source={require('../../assets/images/Art.Google.png')}
+            height={20}
+            width={20}
+          />
+          <Typograph customStyle={{fontWeight: '800', fontSize: Metrics[16]}}>
+            {Config.FIREBASE_GOOGLE_KEY}
+          </Typograph>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default RegisterScreen;
